db4f523e78dd6bff08c4e54fcf376c7a60188e78
twothreezero <realtwothreezero@gmail.com>
2024-11-21 16:21:51 +0900

    update: promotion slide

--- js/main.js
+++ js/main.js
@@ -58,3 +58,20 @@ new Swiper(".notice-line .swiper", {
   autoplay: true,
   loop: true,
 });
+new Swiper(".promotion .swiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".promotion .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".promotion .swiper-button-prev",
+    nextEl: ".promotion .swiper-button-next",
+  },
+});
