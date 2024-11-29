54cce16823a9a4d35db83ef70332ebdb97952448
twothreezero <realtwothreezero@gmail.com>
2024-11-29 15:21:48 +0900

    update: Seperate common module

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// Search Handler
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-
-searchEl.addEventListener("click", function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener("focus", function () {
-  searchEl.classList.add("focused");
-  searchInputEl.setAttribute("placeholder", "통합검색");
-});
-
-searchInputEl.addEventListener("blur", function () {
-  searchEl.classList.remove("focused");
-  searchInputEl.setAttribute("placeholder", "");
-});
-
 // Badges style
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
@@ -150,7 +132,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-// FOOTER
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
